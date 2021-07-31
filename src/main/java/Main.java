de0f048dd92e06e023c6dd84970dc8ee7bce7f21
kdzumba <knowledgedzumba@gmail.com>
2021-08-01 00:48:38 +0200

    triangle version 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+import render.Mesh;
+import render.MeshLoader;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 
@@ -6,8 +13,23 @@ public class Main {
         ApplicationWindow app = new ApplicationWindow(640, 480, "Test Window");
 
         long window = app.getWindow();
+        GL.createCapabilities();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        float[] vertices = {-0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0f, 0.5f, 0f};
+        int[] indices = {0, 1, 2};
+
+        Mesh triangleMesh = MeshLoader.createMesh(vertices, indices);
 
         while(!glfwWindowShouldClose(window)){
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            GL30.glBindVertexArray(triangleMesh.getVaoID());
+            GL20.glEnableVertexAttribArray(0);
+            GL11.glDrawElements(GL11.GL_TRIANGLES, triangleMesh.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+            GL20.glDisableVertexAttribArray(0);
+            GL30.glBindVertexArray(0);
+
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
---
+++ src/main/java/render/Mesh.java
@@ -0,0 +1,19 @@
+package render;
+
+public class Mesh {
+    private int vao;
+    private int vertices;
+
+    public Mesh(int vao, int vertex){
+        this.vao = vao;
+        this.vertices = vertex;
+    }
+
+    public int getVaoID(){
+        return vao;
+    }
+
+    public int getVertexCount(){
+        return vertices;
+    }
+}
